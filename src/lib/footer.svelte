716fd03c339e5a3c9fe44e6d548d720b29ee5f7c
Oein <oein0219@gmail.com>
2023-09-28 01:28:06 +0900

    add footer

---
+++ src/lib/footer.svelte
@@ -0,0 +1,30 @@
+<footer>
+	<div class="container">
+		<h1>All News In One</h1>
+		<div>사이트 맵</div>
+		<a href="/all">- 모든 뉴스</a>
+		<a href="/search">- 검색</a>
+	</div>
+</footer>
+
+<style lang="scss">
+	footer {
+		background: #1f1f1f;
+		width: 100%;
+		padding: 3rem 0px;
+		color: #fff;
+
+		h1 {
+			margin: 0px;
+			margin-bottom: 0.5rem;
+			padding-bottom: 0.5rem;
+			border-bottom: 1.5px solid #ccc;
+		}
+
+		a {
+			display: block;
+			color: #e4e4e4;
+			margin: 0.3rem;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import '@/styles/global.css';
 
 	import Header from '@/lib/header.svelte';
+	import Footer from '@/lib/footer.svelte';
 </script>
 
 <div class="App">
 	<Header />
 	<slot />
+	<Footer />
 </div>
