6b497679eb17cc31e6c125afddc84b3343c26db3
Oein <oein0219@gmail.com>
2023-09-28 01:28:10 +0900

    add creating page

---
+++ src/routes/all/+page.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+	<div>사이트 리뉴얼 중 입니다.</div>
+	<div>더 좋은 기능으로 찾아 뵙도록 하겠습니다.</div>
+	<div>기간 : 2023.08.12 ~</div>
+</div>
+
+<style lang="scss">
+	.container {
+		padding: 3rem 0px;
+		div {
+			text-align: center;
+			font-size: 1.2rem;
+		}
+	}
+</style>
